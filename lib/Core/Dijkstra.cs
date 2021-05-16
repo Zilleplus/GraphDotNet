a9dedb736e164d321b9e9e25586eedde89d7ccd7
Willem Melis <willem.melis@outlook.com>
2021-05-16 13:23:56 +0200

    Dijkstra shortest path
    * Added dijkstra shortest path.

---
+++ lib/Core/Dijkstra.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Core
+{
+    public static partial class Algorithm
+    {
+        internal class DijkstraDistanceComparer : IComparer<Edge>
+        {
+            private readonly int[] distances;
+
+            public DijkstraDistanceComparer(int[] distances)
+            {
+                this.distances = distances;
+            }
+
+            public int Compare(Edge x, Edge y)
+            {
+                if (distances[x.Stop] == distances[y.Stop]) { return 0; }
+                if (distances[x.Stop] < distances[y.Stop]) { return -1; }
+                if (distances[x.Stop] > distances[y.Stop]) { return 1; }
+
+                throw new NotImplementedException();
+            }
+        }
+
+        public record DijkstraSolution(int[] Previous, int[] Distances)
+        {
+            public ReadOnlyCollection<int> Path(int target)
+            {
+                var prev = Previous[target];
+                var path = new List<int> { target };
+                while (target != prev)
+                {
+                    path.Add(prev);
+                    target = prev;
+                    prev = Previous[target];
+                }
+                path.Reverse();
+
+                return path.AsReadOnly();
+            }
+
+            public int Distance(int target)
+                => Distances[target];
+        }
+
+        public static DijkstraSolution DijkStra(ITraversable graph, int source)
+        {
+            var numberOfVertices = graph.GetVertices().Max() + 1;
+
+            var distances = Enumerable.Range(0, numberOfVertices).Select(n => int.MaxValue).ToArray();
+            var previous = distances.ToArray();
+            var knowns = new HashSet<int>();
+            var t = new C5.IntervalHeap<Edge>(new EdgeWeightComparer());
+
+            knowns.Add(source);
+            distances[source] = 0;
+            previous[source] = source;
+            foreach (var e in graph.GetAdjacentEdges(source)) { t.Add(e); }
+
+            while (!t.IsEmpty)
+            {
+                var min = t.FindMin();
+                t.DeleteMin();
+
+                // Default value of distances = Int.Max
+                var newDistance = distances[min.Start] + min.Weight;
+                if (newDistance < distances[min.Stop])
+                {
+                    previous[min.Stop] = min.Start;
+                    distances[min.Stop] = newDistance;
+                }
+
+                if (!knowns.Contains(min.Stop)) // If this edge reaches an undiscovered node.
+                {
+                    knowns.Add(min.Stop);
+                    foreach (var e in graph.GetAdjacentEdges(min.Stop))
+                    {
+                        t.Add(e);
+                    }
+                }
+            }
+
+            return new DijkstraSolution(
+                Distances: distances,
+                Previous: previous);
+        }
+    }
+}
---
+++ test/Core.Test/DijkstraTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+using Xunit;
+
+namespace Core.Test
+{
+    public class DijkstraTests
+    {
+        [Theory]
+        [MemberData(nameof(PathCases))]
+        public static void GivenGraphFindDijkstraSolution(string name, int target, int[] expectedPath, int expectedCost)
+        {
+            Debug.Write($"Test: {name}.");
+
+            var graph = ExampleGraph.Create();
+
+            var dijkstra = Algorithm.DijkStra(graph, source: 0);
+
+            var path = dijkstra.Path(target);
+            var cost = dijkstra.Distance(target);
+
+            Assert.Equal(expected: expectedPath, actual: path);
+            Assert.Equal(expected: expectedCost, actual: cost);
+        }
+
+        public static TheoryData<string, int, int[], int> PathCases()
+        {
+            return new TheoryData<string, int, int[], int>
+            {
+                {
+                    "single edge path",
+                    3,
+                    new [] { 0,3 },
+                    7 // direct connection via the edge (0,3) with weight 7
+                },
+                {
+                    "double edge path",
+                    2,
+                    new [] { 0,3, 2 },
+                    7+4
+                },
+                {
+                    "long path",
+                    6,
+                    new [] { 0,3, 5,6 },
+                    7+3 + 2
+                }
+            };
+        }
+    }
+}
